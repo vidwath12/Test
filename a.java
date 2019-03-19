c2d86408b63fe0817ed032be7050910f55d8f34a
vidwath12 <33801922+vidwath12@users.noreply.github.com>
2019-03-19 14:45:07 +0530

    Done with the current version

--- a.java
+++ a.java
@@ -4,6 +4,7 @@ System.out.println("hello,Welcome to Git")
 System.out.println("new Commit,just check it")
   System.out.println("change 1")
 System.out.println("change 2")
+  System.out.println("Done with the current version")
 }
 
 }
