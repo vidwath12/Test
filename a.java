59e61912eb5635ed85418aaaac770ea35caf5e9e
vidwath12 <33801922+vidwath12@users.noreply.github.com>
2019-03-19 14:42:14 +0530

    change 4

--- a.java
+++ a.java
@@ -1,9 +1,10 @@
 public class a{
 public static void main(String args[]){
-System.out.println("hello,Welcome to Git")
-System.out.println("new Commit,just check it")
-  System.out.println("change 1")
-System.out.println("change 2")
+System.out.println("hello,Welcome to Git");
+System.out.println("new Commit,just check it");
+  System.out.println("change 1");
+System.out.println("change 2");
+  System.out.println("change3");
 }
 
 }
